a590105d56411fc47238970a56adbc88cc9e241f
saad noor <ssaadn.555@gmail.com>
2023-10-17 11:48:33 +0600

    mapping done

--- codes/XMLTree.py
+++ codes/XMLTree.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
+import math
 
 class XMLTree:
     def __init__(self, node=None, filepath:str=None) -> None:
+        coord = ['x1', 'y1', 'x2', 'y2']
+        idx = 0
         if filepath != None and node==None:
             tree = ET.parse(filepath)
             node = list(list(tree.getroot())[1])[0]
@@ -10,30 +13,46 @@ class XMLTree:
         # self.attributes = []
         self.attributes = {}
         self.children = []
-        self.relations = {}
         for x in list(node):
             if x.tag=='children':
                 chld = XMLTree(x)
                 self.children.append(chld)
             else:
                 # self.attributes.append(x)
-                self.attributes[x.tag] = x.text
+                key = x.tag
+                if x.tag == 'bounds':
+                    key = coord[idx]
+                    idx += 1
+                self.attributes[key] = x.text
 
-    def dfs(self, debug:bool=False, findRel:bool=False, nodes:list=[])->list:
+    def dfs(self, debug:bool=False, class_text:str='', findRel:bool=False, nodes:list=[], des_nodes:list=[])->list:
         nodes.append(self)
         if debug:
             print(self.attributes)
             x = input()
+        
+        '''The following part check whether the current node belongs
+        to the desired class'''
+        try:
+            for text in class_text:
+                if text in self.attributes['class']:
+                    des_nodes.append(self)
+                    break
+        except:
+            pass
+        '''
+        # OBSOLETE
         if findRel:
             # print('yo')
             self.relations.update(self.findRelations())
+        '''
         for child in self.children:
-            nodes = child.dfs(debug=debug, nodes=nodes, findRel=findRel)
+            nodes, des_nodes = child.dfs(debug=debug, nodes=nodes, findRel=findRel, des_nodes=des_nodes)
         
-        return nodes
+        return nodes, des_nodes
 
-    def bfs(self, debug:bool=False, findRel:bool=False)->list:
-        nodes, queue = [], []
+    def bfs(self, debug:bool=False, class_text:list=[], findRel:bool=False)->list:
+        nodes, queue, class_valid = [], [], []
         queue.append((0, self))
         while len(queue)!=0:
             cur = queue[0]
@@ -41,13 +60,34 @@ class XMLTree:
             nodes.append(cur[1])
             if debug:
                 print(cur[0], cur[1].node)
+            '''The following part checks for desired classes'''
+            try:
+                for text in class_text:
+                    if text in cur[1].attributes['class']:
+                        class_valid.append(cur[1])
+                        break
+            except:
+                pass
+            '''
+            # OBSOLETE
             if findRel:
                 self.relations.update(cur[1].findRelations())
+            '''
             for child in cur[1].children:
                 queue.append((cur[0]+1, child))
-        
-        return nodes
+        return nodes, class_valid
     
+    def getDistance(self, oth: object):
+        c1 = self.getCenter()
+        c2 = oth.getCenter()
+        return math.sqrt((c1[0]-c2[0])**2+(c1[1]-c2[1])**2)
+
+    def getCenter(self):
+        return ((int(self.attributes['x1'])+int(self.attributes['x2']))/2,
+                (int(self.attributes['y1'])+int(self.attributes['y2']))/2)
+
+    '''
+    # OBSOLETE
     def findRelations(self, label_texts:list=['TextView'], input_texts:list=['EditText']):
         label_box_map = {}
         for i in range(len(self.children)):
@@ -63,6 +103,7 @@ class XMLTree:
                 continue
         
         return label_box_map
+    '''
 
 
 
--- codes/parser.py
+++ codes/parser.py
@@ -5,18 +5,54 @@ class Parser:
         self.filepath = file
         self.tree = XMLTree(filepath=file)
 
+    '''
+    # OBSOLETE
     def traverse(self, strategy:str='bfs', debug:bool=False, findRel:bool=False):
         self.tree.bfs(debug=debug, findRel=findRel) if strategy=='bfs'\
               else self.tree.dfs(debug=debug, findRel=findRel)
+    '''
 
+    def findClass(self, class_text:list, strategy='bfs'):
+        strat_map = {
+            'bfs': self.tree.bfs,
+            'dfs': self.tree.dfs
+        }
+        all, valid = strat_map[strategy](class_text=class_text)
+        return valid
 
-if __name__=='__main__':
-    filepath = r'F:\spl3\Credential-Mapping\dataset\xmls\formUI1.xml'
-    tp = Parser(filepath)
-    tp.traverse(strategy='bfs', debug=False, findRel=True)
+'''
+# OBSOLETE
+def exp1(tp: Parser):
+    tp.traverse(strategy='bfs', debug=True, findRel=True)
     for key in tp.tree.relations.keys():
         print('label:')
         print(key.attributes)
         print('**********')
         print('field')
         print(tp.tree.relations[key].attributes)
+'''
+
+def exp2(tp: Parser):
+    labels = tp.findClass(class_text=['TextView'])
+    input_boxes = tp.findClass(class_text=['EditText'])
+    # print(len(labels), len(input_boxes))
+    # for box in input_boxes:
+    #     print(box.getCenter())
+    input_map = {}
+    for label in labels:
+        dist = 9999
+        for box in input_boxes:
+            cur_dist = label.getDistance(box)
+            if cur_dist<dist:
+                dist = cur_dist
+                input_map[label] = box
+    for key in input_map.keys():
+        print('label:')
+        print(key.attributes)
+        print('**************')
+        print(input_map[key].attributes)
+
+if __name__=='__main__':
+    filepath = r'F:\spl3\Credential-Mapping\dataset\xmls\loginUI.xml'
+    tp = Parser(filepath)
+    exp2(tp)
