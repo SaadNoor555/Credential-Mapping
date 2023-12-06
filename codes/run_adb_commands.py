99b379a14ba59412d312af58b0aea6cf5fcb5279
saad noor <ssaadn.555@gmail.com>
2023-12-06 18:11:27 +0600

    adb command added

---
+++ codes/run_adb_commands.py
@@ -0,0 +1,66 @@
+import os
+
+class AdbCommand:
+    def __init__(self, packagename) -> None:
+        self.package = packagename
+        self.start_app()
+
+    def swipe_event(self, start, end, duration=50):
+        command = f'input swipe {start[0]} {start[1]} \
+            {end[0]} {end[1]} {duration}'
+        test_adb_shell(command)
+
+    def touch_event(self, coord):
+        command = f'input tap {coord[0]} {coord[1]}'
+        test_adb_shell(command)
+
+    def key_press_event(self, key, longpress=False):
+        key_dict = {
+            'up': 280,
+            'down': 281,
+            'left': 282,
+            'right': 283,
+            'caps_lock': 115,
+            'soft_left': 1,
+            'soft_right': 2,
+            'dpad_up': 19,
+            'dpad_down': 20,
+            'dpad_left': 21,
+            'dpad_right': 22,
+            'dpad_center': 23,
+            'volume_up': 24,
+            'volume_down': 25,
+            'power': 26,
+            'clear': 28,
+            'enter': 66,
+            'del': 67,
+            'escape': 111,
+        }
+        test_adb_shell(f'input keyevent {key_dict[key]}')
+
+    def type_event(self, message):
+        test_adb_shell(f'input text {message}')
+
+
+    def run_monkey(self, appname):
+        pass
+    def start_app(self):
+        command = f'am start {self.package}'
+        test_adb_shell(command)
+
+    def get_ui_info(self, outputfile='window.xml'):
+        command = f'uiautomator dump {outputfile}'
+        test_adb_shell(command)
+
+
+def test_adb_shell(command):
+    os.system(f'adb shell {command}')
+
+
+if __name__=='__main__':
+    # test_adb_shell('input swipe 100 1000 100 200')
+    # test_adb_shell('input keyevent 28')
+    # key_press_event('del')
+    commander = AdbCommand('com.google.android.contacts')
+    commander.get_ui_info()
+    # start_app('com.google.android.contacts')
